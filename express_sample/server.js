d9fcd774ccf957b26746879cceb09b817b710fff
220003 <220003pb@yse-c.net>
2023-10-04 11:55:07 +0900

    server commit

--- express_sample/server.js
+++ express_sample/server.js
@@ -4,7 +4,28 @@ const dotenv = require('dotenv')//dotenvモジュールの読み込み
 //dotenvの設定読み込み
 dotenv.config()
 const HOST = process.env.HOST
-const POST = process.env.PORT
+const PORT = process.env.PORT
 
 console.log(HOST)
 console.log(PORT)
+
+//サーバー作成
+const app = express()
+
+//getリクエスト
+app.get('/',(req,res) => {//URLのパス,(リクエスト,レスポンス)
+    console.log(req.body)
+    console.log(req.url)
+    console.log(req.query)
+
+    //レスポンスの処理
+    res.send('Hello!!!')
+})
+
+//サーバ停止　ctrl+c
+//サーバ待機(Listen)
+app.listen(PORT,HOST,() => {
+    console.log(HOST)
+    console.log(PORT)
+    console.log('wait...')
+})
